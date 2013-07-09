6cd190a999b30e1400ada861cd619671b49be56a
Dennis Kanygin <denis.kanygin@gmail.com>
2013-07-09 07:01:18 +0000

    initial working version of grader.js and checks.json

--- grader.js
+++ grader.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-
 /*
 Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development
@@ -29,15 +28,15 @@ var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
-    var instr = infile.toString();
-    if(!fs.existsSync(instr)){
-	console.log("%s does not exist. Exiting.", instr);
-	process.exit(1); //http://nodejs.org/api/process.html#process_process_exit_code
-    }
-    return instr;
+  var instr = infile.toString();
+  if(!fs.existsSync(instr)) {
+    console.log("%s does not exist. Exiting.", instr);
+    process.exit(1); //http://nodejs.org/api/process.html#process_process_exit_code
+  }
+  return instr;
 };
 
-var cheerioHtmlFile = function(htlfile) {
+var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
@@ -50,10 +49,11 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for (var ii in checks) {
-	var present = $(checks[ii]).length > 9;
+	var present = $(checks[ii]).length > 0;
 	out[checks[ii]] = present;
     }
     return out;
+};
 
 var clone = function(fn) {
     // Workaround for commander.js issue.
@@ -64,8 +64,8 @@ var clone = function(fn) {
 if(require.main == module) {
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
+	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
